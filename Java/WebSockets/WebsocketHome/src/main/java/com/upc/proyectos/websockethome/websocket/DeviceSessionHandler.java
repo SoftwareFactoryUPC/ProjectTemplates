937bcf595d4f6dcb7ab668d172b60b877cea1367
Roy Taza <roytazarojas@gmail.com>
2016-05-29 17:41:44 -0500

    WebSockets
    Introducción y guía de WebSockets para crear una aplicación de
    administración de dispositivos WebSockets Home.

---
+++ Java/WebSockets/WebsocketHome/src/main/java/com/upc/proyectos/websockethome/websocket/DeviceSessionHandler.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.upc.proyectos.websockethome.websocket;
+
+import com.upc.proyectos.websockethome.model.Device;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.JsonObject;
+import javax.json.spi.JsonProvider;
+import javax.websocket.Session;
+
+/**
+ *
+ * @author Roy
+ */
+@ApplicationScoped
+public class DeviceSessionHandler {
+
+    private int deviceId = 0;
+    private final Set sessions = new HashSet<>();
+    private final Set devices = new HashSet<>();
+
+    public void addSession(Session session) {
+        sessions.add(session);
+        for (Object device : devices) {
+            JsonObject addMessage = createAddMessage((Device) device);
+            sendToSession(session, addMessage);
+        }
+    }
+
+    public void removeSession(Session session) {
+        sessions.remove(session);
+    }
+
+    public List getDevices() {
+        return new ArrayList<>(sessions);
+    }
+
+    public void addDevice(Device device) {
+        device.setId(deviceId);
+        devices.add(device);
+        deviceId++;
+        JsonObject addMessage = createAddMessage(device);
+        sendToAllConnectedSessions(addMessage);
+    }
+
+    public void removeDevice(int id) {
+        Device device = getDeviceById(id);
+        if (device != null) {
+            devices.remove(device);
+            JsonProvider provider = JsonProvider.provider();
+            JsonObject removeMessage = provider.createObjectBuilder()
+                    .add("action", "remove")
+                    .add("id", id)
+                    .build();
+            sendToAllConnectedSessions(removeMessage);
+        }
+    }
+
+    public void toggleDevice(int id) {
+        JsonProvider provider = JsonProvider.provider();
+        Device device = getDeviceById(id);
+        if (device != null) {
+            if ("On".equals(device.getStatus())) {
+                device.setStatus("Off");
+            } else {
+                device.setStatus("On");
+            }
+            JsonObject updateDevMessage = provider.createObjectBuilder()
+                    .add("action", "toggle")
+                    .add("id", device.getId())
+                    .add("status", device.getStatus())
+                    .build();
+            sendToAllConnectedSessions(updateDevMessage);
+        }
+    }
+
+    private Device getDeviceById(int id) {
+        for (Object device : devices) {
+            if (((Device) device).getId() == id) {
+                return (Device) device;
+            }
+        }
+        return null;
+    }
+
+    private JsonObject createAddMessage(Device device) {
+        JsonProvider provider = JsonProvider.provider();
+        JsonObject addMessage = provider.createObjectBuilder()
+                .add("action", "add")
+                .add("id", device.getId())
+                .add("name", device.getName())
+                .add("type", device.getType())
+                .add("status", device.getStatus())
+                .add("description", device.getDescription())
+                .build();
+        return addMessage;
+    }
+
+    private void sendToAllConnectedSessions(JsonObject message) {
+        for (Object session : sessions) {
+            sendToSession((Session) session, message);
+        }
+    }
+
+    private void sendToSession(Session session, JsonObject message) {
+        try {
+            session.getBasicRemote().sendText(message.toString());
+        } catch (IOException ex) {
+            sessions.remove(session);
+            Logger.getLogger(DeviceSessionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
